d204b558239aa25f8e14ac26d936c253ae47bbfa
Markus <e@markwil.so>
2019-03-23 16:19:56 +0000

    Titlebar component

---
+++ src/Components/TitleBar/TitleBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewProjectButton from '../NewProjectButton/NewProjectButton'
+import './TitleBar.css'
+
+const TitleBar = () => {
+  return (
+    <div className='TitleBar'>
+      <NewProjectButton />
+    </div>
+  )
+}
+
+export default TitleBar
