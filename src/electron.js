86efcd73483442469090040ecce036c55a0e64ef
Markus <e@markwil.so>
2019-03-23 16:17:23 +0000

    electron init

---
+++ src/electron.js
@@ -0,0 +1,41 @@
+/* eslint-disable*/
+
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    width: 300,
+    height: 600,
+    minWidth: 400,
+    minHeight: 300,
+    frame: false,
+    titleBarStyle: 'hiddenInset',
+    backgroundColor: '#17002F',
+    resizable: false
+  })
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+
+    if (isDev) {
+      mainWindow.openDevTools()
+
+      const {
+        default: installExtension,
+        REACT_DEVELOPER_TOOLS,
+        REDUX_DEVTOOLS
+      } = require('electron-devtools-installer')
+
+      installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
+        .then(name => console.log(`Added Extension:  ${name}`))
+        .catch(err => console.log('An error occurred: ', err))
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
