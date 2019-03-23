75326455ae045b08d133a87eb341c3ed6e39a88c
Markus <e@markwil.so>
2019-03-23 16:19:28 +0000

    Toolbar

---
+++ src/Components/ToolBar/ToolBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewProjectButton from '../NewProjectButton/NewProjectButton'
+import './ToolBar.css'
+
+const ToolBar = () => {
+  return (
+    <div className='ToolBar'>
+      <NewProjectButton />
+    </div>
+  )
+}
+
+export default ToolBar
