e79e3edd839108504b11fef9d95d81f5ccb7bdad
Markus <e@markwil.so>
2019-03-23 16:41:28 +0000

    Added defaults to config

---
+++ src/config.js
@@ -0,0 +1,23 @@
+export const DEFAULT_WORKFLOW = [
+  'Master',
+  'Wireframes',
+  'User Journeys'
+];
+
+export const DEFAULT_TAGS = [
+  'Phone',
+  'Tablet',
+  'Desktop'
+];
+
+export const DEFAULT_FOLDERS = [
+  'Archive',
+  'Client',
+  'Proposal'
+]
+
+export default {
+  DEFAULT_WORKFLOW,
+  DEFAULT_TAGS,
+  DEFAULT_FOLDERS
+}
