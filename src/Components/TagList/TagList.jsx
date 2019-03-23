9ebebcc25b20161b58423dc07445147c8139b131
Markus <e@markwil.so>
2019-03-23 17:34:21 +0000

    Styled taglist component

--- src/Components/TagList/TagList.jsx
+++ src/Components/TagList/TagList.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Check } from 'react-feather'
 import './TagList.css'
 
-const TagList = () => {
+const TagList = (props) => {
   return (
     <div className='TagList'>
-
+      {props.defaultTags.map((tag, index) => (
+        <label
+          className='TagListItem'
+          key={index} >
+          <input
+            hidden
+            type='checkbox'
+            value={tag} />
+          <span>{tag}<Check className='Icon'/></span>
+        </label>
+      ))}
     </div>
   )
 }
