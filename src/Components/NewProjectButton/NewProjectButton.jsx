111d37a5b9edf0e9e950dfd3a55a412fb871ab43
Markus <e@markwil.so>
2019-03-23 16:20:12 +0000

    New project button component

---
+++ src/Components/NewProjectButton/NewProjectButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewProjectButton.css'
+
+const NewProjectButton = () => {
+  return (
+    <button className='NewProjectButton'>
+      Create New
+    </button>
+  )
+}
+
+export default NewProjectButton
