5d1b9032b8a7b71fd03530dcfd93847974ccdabe
Markus <e@markwil.so>
2019-03-24 00:41:09 +0000

    Added project location component to app file

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,20 +1,28 @@
+const { app, dialog } = require('electron').remote
 import React, { Component } from 'react'
 import { DEFAULT_TAGS } from '../config'
+import { selectDirectory } from '../Utils/SelectDirectory'
 import TitleBar from '../Components/TitleBar/TitleBar'
 import ToolBar from '../Components/ToolBar/ToolBar'
 import NameProject from '../Components/NameProject/NameProject'
 import TagList from '../Components/TagList/TagList'
 import AddTag from '../Components/AddTag/AddTag'
+import ProjectLocation from '../Components/ProjectLocation/ProjectLocation'
 import './App.css'
+
+const HOME_DIRECTORY = app.getPath('home') + '/Projects';
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       tags: DEFAULT_TAGS,
-      switch: false
+      switch: false,
+      projectName: ''
     }
   }
+
+  nameHandler = event => this.setState({projectName: event.target.value})
 
   addTagSwitchHandler = () => this.setState({switch: !this.state.switch})
 
@@ -29,12 +37,16 @@ export default class App extends Component {
     return (
       <div className='App'>
         <TitleBar />
-        <NameProject />
+        <NameProject change={() => this.nameHandler(event)}/>
         <TagList defaultTags={this.state.tags} />
         <AddTag
           switch={this.state.switch}
           handleSwitch={this.addTagSwitchHandler}
-          handleAdd={() => this.addTagHandler(event)}/>
+          handleAdd={() => this.addTagHandler(event)} />
+        <ProjectLocation
+          location={HOME_DIRECTORY}
+          name={this.state.projectName}
+          clicked={() => selectDirectory()}/>
       </div>
     )
   }
