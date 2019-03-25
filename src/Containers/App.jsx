5edf237230373d8261aef72373ec64d7e7c4d526
Markus <e@markwil.so>
2019-03-25 20:28:21 +0000

    removed add tag component from app file

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,30 +1,48 @@
-const { app, dialog } = require('electron').remote
 import React, { Component } from 'react'
+const { app, dialog } = require('electron').remote
 import { DEFAULT_TAGS } from '../config'
-import { selectDirectory } from '../Utils/SelectDirectory'
 import TitleBar from '../Components/TitleBar/TitleBar'
 import ToolBar from '../Components/ToolBar/ToolBar'
 import NameProject from '../Components/NameProject/NameProject'
 import TagList from '../Components/TagList/TagList'
-import AddTag from '../Components/AddTag/AddTag'
 import ProjectLocation from '../Components/ProjectLocation/ProjectLocation'
+import CreateProject from '../Components/CreateProject/CreateProject'
+import { createDirectory } from '../Utils/CreateDirectory'
 import './App.css'
 
-const HOME_DIRECTORY = app.getPath('home') + '/Projects';
+const HOME_DIRECTORY = app.getPath('home') + '/Projects'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       tags: DEFAULT_TAGS,
-      switch: false,
-      projectName: ''
+      checkedTags: [],
+      buttonSwitch: false,
+      projectName: '',
+      projectLocation: HOME_DIRECTORY
     }
   }
 
   nameHandler = event => this.setState({projectName: event.target.value})
 
-  addTagSwitchHandler = () => this.setState({switch: !this.state.switch})
+  checkedTagsHandler = (event, props) => {
+    let setCheckedTags;
+    if (event.target.checked) {
+      setCheckedTags = [...this.state.checkedTags, event.target.value];
+    }
+    else {
+      setCheckedTags = [...this.state.checkedTags].filter(
+        index => this.state.checkedTags.indexOf(index) !== this.state.checkedTags.indexOf(props)
+      );
+    }
+    console.log(setCheckedTags)
+    this.setState({
+      checkedTags: setCheckedTags
+    })
+  };
+
+  addTagSwitchHandler = () => this.setState({buttonSwitch: !this.state.buttonSwitch})
 
   addTagHandler = event => {
     const setTags = [...this.state.tags, event.target.value]
@@ -33,20 +51,34 @@ export default class App extends Component {
     })
   }
 
+  selectDirectory = () => {
+    const options = {
+      defaultPath: app.getPath('home'),
+      buttonLabel: 'Choose',
+      properties: ['openDirectory']
+    }
+    const selectedDirectory = dialog.showOpenDialog(options)
+    this.setState({
+      projectLocation: selectedDirectory
+    })
+  }
+
   render() {
     return (
       <div className='App'>
         <TitleBar />
-        <NameProject change={() => this.nameHandler(event)}/>
-        <TagList defaultTags={this.state.tags} />
-        <AddTag
-          switch={this.state.switch}
+        <NameProject change={this.nameHandler}/>
+        <TagList
+          defaultTags={this.state.tags}
+          clicked={this.checkedTagsHandler}
+          buttonSwitch={this.state.buttonSwitch}
           handleSwitch={this.addTagSwitchHandler}
-          handleAdd={() => this.addTagHandler(event)} />
+          handleAdd={this.addTagHandler} />
         <ProjectLocation
-          location={HOME_DIRECTORY}
+          location={this.state.projectLocation}
           name={this.state.projectName}
-          clicked={() => selectDirectory()}/>
+          clicked={this.selectDirectory} />
+        <CreateProject clicked={() => createDirectory(this.state.checkedTags, this.state.projectName, this.state.projectLocation)}/>
       </div>
     )
   }
