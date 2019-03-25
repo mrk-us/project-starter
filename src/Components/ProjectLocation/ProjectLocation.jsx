a470691a462d35ede4e483d7f8deab6dc9f137c5
Markus <e@markwil.so>
2019-03-25 21:49:37 +0000

    Added section title to project location

--- src/Components/ProjectLocation/ProjectLocation.jsx
+++ src/Components/ProjectLocation/ProjectLocation.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Aux from '../../Hoc/Aux'
 import './ProjectLocation.css'
 
 const ProjectLocation = props => {
   return (
-    <button
-      className='ProjectLocation'
-      onClick={props.clicked}>
-      {props.location}/{props.name}
-    </button>
+    <Aux>
+      <span className='SectionTitle'>Project Location</span>
+      <button
+        className='ProjectLocation'
+        onClick={props.clicked}>
+        {props.location}/{props.name}
+      </button>
+  </Aux>
   )
 }
 
