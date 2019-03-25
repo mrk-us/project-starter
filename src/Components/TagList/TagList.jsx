5d30577de215d571d4702ff11388767994a97e32
Markus <e@markwil.so>
2019-03-25 20:26:56 +0000

    Add tags button added to tag list component

--- src/Components/TagList/TagList.jsx
+++ src/Components/TagList/TagList.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import AddTag from '../AddTag/AddTag'
 import { Check } from 'react-feather'
 import './TagList.css'
 
-const TagList = (props) => {
+const TagList = props => {
   return (
     <div className='TagList'>
       <span>Target platform</span>
       {props.defaultTags.map((tag, index) => (
         <label
           className='TagListItem'
+          onClick={event => props.clicked(event, tag)}
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
+              event.target.click()
+            }
+          }}
           tabIndex={index + 1}
           key={index} >
           <input
@@ -18,6 +25,10 @@ const TagList = (props) => {
           <span>{tag}<Check className='Icon'/></span>
         </label>
       ))}
+      <AddTag
+        buttonSwitch={props.buttonSwitch}
+        handleSwitch={props.handleSwitch}
+        handleAdd={props.handleAdd} />
     </div>
   )
 }
